f589add5c55057660fe6eb103e68726e58602e3f
Nikita Sirovsky <nicksirovsky@gmail.com>
2021-10-22 02:45:31 +0300

    Add the circled state to the leafy text button

--- src/lib/shared_widget/leafy_text_button.dart
+++ src/lib/shared_widget/leafy_text_button.dart
@@ -10,16 +10,58 @@ class LeafyTextButton<TTheme extends LeafyTheme> extends ThemedWidget<TTheme> {
     this.onPressed,
     this.onLongPressed,
     this.backgroundColor,
-  }) : super(key: key);
+  })  : _isCircled = false,
+        super(key: key);
+
+  const LeafyTextButton.circled({
+    Key? key,
+    required this.child,
+    this.onPressed,
+    this.onLongPressed,
+    this.backgroundColor,
+  })  : _isCircled = true,
+        super(key: key);
+
+  static const _defaultCircledSize = 48.0;
+  static const _containerSize = _defaultCircledSize;
 
   final Color? backgroundColor;
   final VoidCallback? onPressed;
   final VoidCallback? onLongPressed;
   final Widget child;
+  final bool _isCircled;
 
   @override
   Widget body(BuildContext context, LeafyTheme theme) {
     final style = ButtonStyle(
+      fixedSize: _isCircled
+          ? MaterialStateProperty.resolveWith(
+              (_) {
+                return const Size(_defaultCircledSize, _defaultCircledSize);
+              },
+            )
+          : null,
+      maximumSize: _isCircled
+          ? MaterialStateProperty.resolveWith(
+              (_) {
+                return const Size(_defaultCircledSize, _defaultCircledSize);
+              },
+            )
+          : null,
+      minimumSize: _isCircled
+          ? MaterialStateProperty.resolveWith(
+              (_) {
+                return const Size(_defaultCircledSize, _defaultCircledSize);
+              },
+            )
+          : null,
+      padding: _isCircled
+          ? MaterialStateProperty.resolveWith(
+              (_) {
+                return EdgeInsets.zero;
+              },
+            )
+          : null,
       foregroundColor: MaterialStateColor.resolveWith(
         (states) {
           if (states.contains(MaterialState.disabled)) {
@@ -37,11 +79,23 @@ class LeafyTextButton<TTheme extends LeafyTheme> extends ThemedWidget<TTheme> {
       ),
     );
 
-    return TextButton(
+    final button = TextButton(
       onPressed: onPressed,
       onLongPress: onLongPressed,
       style: style,
       child: child,
+    );
+
+    if (!_isCircled) {
+      return button;
+    }
+
+    return Container(
+      width: _containerSize,
+      height: _containerSize,
+      decoration: const BoxDecoration(shape: BoxShape.circle),
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      child: Center(child: button),
     );
   }
 }
