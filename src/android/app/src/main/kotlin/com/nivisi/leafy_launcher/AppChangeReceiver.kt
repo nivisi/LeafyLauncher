c84bb8a049530c23cf904b3b205c05e4ade2ad0a
Nikita Sirovsky <nicksirovsky@gmail.com>
2021-11-14 19:43:29 +0200

    Fix Android build error

--- src/android/app/src/main/kotlin/com/nivisi/leafy_launcher/AppChangeReceiver.kt
+++ src/android/app/src/main/kotlin/com/nivisi/leafy_launcher/AppChangeReceiver.kt
@@ -3,21 +3,14 @@ package com.nivisi.leafy_launcher
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import java.io.Serializable
 
-
-class AppChangeReceiver(private var handler: StreamHandlerParams<Map<String, Serializable>>) : BroadcastReceiver() {
+public class AppChangeReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
         val packageName = intent?.data?.encodedSchemeSpecificPart ?: return
 
-        val removed = intent.action == Intent.ACTION_PACKAGE_REMOVED
+        val isRemoved = intent.action == Intent.ACTION_PACKAGE_REMOVED
 
-        handler.dispatch(
-            mapOf(
-                "package" to packageName,
-                "isRemoved" to removed
-            )
-        )
+        MainActivity.self.dispatchAppChangedEvent(packageName, isRemoved)
     }
 }
