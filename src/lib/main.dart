853d43cbcc58e4351550fc05d97c4f589b4f0d09
Nikita Sirovsky <nicksirovsky@gmail.com>
2021-12-16 21:24:40 +0200

    Leave a comment

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -6,6 +6,9 @@ import 'applications/notes/leafy_notes.dart';
 
 const _appChannel = MethodChannel('com.nivisi.leafy_launcher/app');
 
+// For a reason we cannot create another entry point,
+// so we need to distinguish the app by calling a method channel.
+
 @pragma('vm:entry-point')
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
