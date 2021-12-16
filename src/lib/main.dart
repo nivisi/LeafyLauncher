ba36a4376955bf12b195c0ee04de4ed217b8e388
Nikita Sirovsky <nicksirovsky@gmail.com>
2021-12-16 20:31:14 +0200

    Mark entry points with pragma

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -3,12 +3,14 @@ import 'package:flutter/material.dart';
 import 'applications/launcher/leafy_launcher.dart';
 import 'applications/notes/leafy_notes.dart';
 
+@pragma('vm:entry-point')
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
   LeafyLauncher.run();
 }
 
+@pragma('vm:entry-point')
 void mainNotes() {
   WidgetsFlutterBinding.ensureInitialized();
 
