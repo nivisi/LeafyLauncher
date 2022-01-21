90086bc0eb99b5e0497b39e0c279bf4c0e55d1e5
Nikita Sirovskiy <nicksirovsky@gmail.com>
2022-01-22 00:47:10 +0200

    Refactor the fab and close the fab on back button

--- src/lib/module/home_notes/notes/folders/widget/fab/home_note_folders_fab.dart
+++ src/lib/module/home_notes/notes/folders/widget/fab/home_note_folders_fab.dart
@@ -1,26 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:leafy_launcher/resources/app_constants.dart';
 import 'package:leafy_launcher/resources/theme/home_theme.dart';
 import 'package:leafy_launcher/resources/theme/leafy_theme.dart';
 import 'package:leafy_launcher/shared_widget/leafy_text_button.dart';
-import 'package:leafy_launcher/shared_widget/themed_state.dart';
+import 'package:leafy_launcher/shared_widget/themed_widget.dart';
 
-class HomeNoteFoldersFab extends StatefulWidget {
-  const HomeNoteFoldersFab({
-    Key? key,
-    this.onCreateNoteTap,
-    this.onCreateFolderTap,
-  }) : super(key: key);
+import '../../home_note_folders_controller.dart';
 
-  final VoidCallback? onCreateNoteTap;
-  final VoidCallback? onCreateFolderTap;
+class HomeNoteFoldersFab extends ThemedWidget {
+  const HomeNoteFoldersFab({Key? key}) : super(key: key);
 
-  @override
-  _HomeNoteFoldersFabState createState() => _HomeNoteFoldersFabState();
-}
-
-class _HomeNoteFoldersFabState
-    extends ThemedState<HomeNoteFoldersFab, HomeTheme> {
   static const _fabSize = 56.0;
   static const _rightFabPadding = kDefaultPadding;
   static const _bottomFabPadding = kDefaultPadding;
@@ -33,103 +23,90 @@ class _HomeNoteFoldersFabState
   static const _bottomButtonPadding =
       _bottomFabPadding + (_fabSize - _buttonSize) / 2;
 
-  bool _isOpened = false;
-
-  void _onFabPressed() {
-    setState(() {
-      _isOpened = !_isOpened;
-    });
-  }
-
   @override
   Widget body(BuildContext context, LeafyTheme theme) {
-    return Stack(
-      children: [
-        IgnorePointer(
-          ignoring: !_isOpened,
-          child: GestureDetector(
-            onPanDown: (_) {
-              setState(() {
-                _isOpened = false;
-              });
-            },
-          ),
-        ),
-        AnimatedPositioned(
-          right: _rightButtonPadding,
-          bottom: _isOpened
-              ? _bottomButtonPadding +
-                  _fabSize +
-                  _buttonSize +
-                  kDefaultPadding / 2.0
-              : _bottomButtonPadding,
-          duration: kDefaultAnimationDuration,
-          curve: kDefaultAnimationCurve,
-          child: LeafyTextButton<HomeTheme>.circled(
-            backgroundColor: theme.leafyColor,
-            onPressed: widget.onCreateNoteTap != null
-                ? () {
-                    setState(() {
-                      _isOpened = false;
-                    });
-                    widget.onCreateNoteTap!.call();
-                  }
-                : null,
-            child: Icon(
-              Icons.description_outlined,
-              color: theme.backgroundColor,
+    return GetBuilder<HomeNoteFoldersController>(
+      id: HomeNoteFoldersController.fabBuidler,
+      builder: (controller) {
+        return Stack(
+          children: [
+            IgnorePointer(
+              ignoring: !controller.isFabOpened,
+              child: GestureDetector(
+                onPanDown: (_) {
+                  controller.closeFab();
+                },
+              ),
             ),
-          ),
-        ),
-        AnimatedPositioned(
-          right: _rightButtonPadding,
-          bottom: _isOpened
-              ? _bottomButtonPadding + _fabSize
-              : _bottomButtonPadding,
-          duration: kDefaultAnimationDuration,
-          curve: kDefaultAnimationCurve,
-          child: LeafyTextButton<HomeTheme>.circled(
-            backgroundColor: theme.leafyColor,
-            onPressed: widget.onCreateFolderTap != null
-                ? () {
-                    setState(() {
-                      _isOpened = false;
-                    });
-                    widget.onCreateFolderTap!.call();
-                  }
-                : null,
-            child: Icon(
-              Icons.folder_outlined,
-              color: theme.backgroundColor,
-            ),
-          ),
-        ),
-        Positioned(
-          right: _rightFabPadding,
-          bottom: _bottomFabPadding,
-          child: FloatingActionButton(
-            foregroundColor: theme.backgroundColor,
-            backgroundColor: theme.leafyColor,
-            onPressed: _onFabPressed,
-            child: AnimatedCrossFade(
-              crossFadeState: _isOpened
-                  ? CrossFadeState.showSecond
-                  : CrossFadeState.showFirst,
+            AnimatedPositioned(
+              right: _rightButtonPadding,
+              bottom: controller.isFabOpened
+                  ? _bottomButtonPadding +
+                      _fabSize +
+                      _buttonSize +
+                      kDefaultPadding / 2.0
+                  : _bottomButtonPadding,
               duration: kDefaultAnimationDuration,
-              firstCurve: kDefaultAnimationCurve,
-              secondCurve: kDefaultAnimationCurve,
-              firstChild: Icon(
-                Icons.add_rounded,
-                color: theme.backgroundColor,
-              ),
-              secondChild: Icon(
-                Icons.close_rounded,
-                color: theme.backgroundColor,
+              curve: kDefaultAnimationCurve,
+              child: LeafyTextButton<HomeTheme>.circled(
+                backgroundColor: theme.leafyColor,
+                onPressed: () {
+                  controller.closeFab();
+                  controller.createNote();
+                },
+                child: Icon(
+                  Icons.description_outlined,
+                  color: theme.backgroundColor,
+                ),
               ),
             ),
-          ),
-        ),
-      ],
+            AnimatedPositioned(
+              right: _rightButtonPadding,
+              bottom: controller.isFabOpened
+                  ? _bottomButtonPadding + _fabSize
+                  : _bottomButtonPadding,
+              duration: kDefaultAnimationDuration,
+              curve: kDefaultAnimationCurve,
+              child: LeafyTextButton<HomeTheme>.circled(
+                backgroundColor: theme.leafyColor,
+                onPressed: () {
+                  controller.closeFab();
+                  controller.createFolder();
+                },
+                child: Icon(
+                  Icons.folder_outlined,
+                  color: theme.backgroundColor,
+                ),
+              ),
+            ),
+            Positioned(
+              right: _rightFabPadding,
+              bottom: _bottomFabPadding,
+              child: FloatingActionButton(
+                foregroundColor: theme.backgroundColor,
+                backgroundColor: theme.leafyColor,
+                onPressed: controller.onFabPressed,
+                child: AnimatedCrossFade(
+                  crossFadeState: controller.isFabOpened
+                      ? CrossFadeState.showSecond
+                      : CrossFadeState.showFirst,
+                  duration: kDefaultAnimationDuration,
+                  firstCurve: kDefaultAnimationCurve,
+                  secondCurve: kDefaultAnimationCurve,
+                  firstChild: Icon(
+                    Icons.add_rounded,
+                    color: theme.backgroundColor,
+                  ),
+                  secondChild: Icon(
+                    Icons.close_rounded,
+                    color: theme.backgroundColor,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
