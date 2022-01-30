f7f8de8483824f3f2d72d9f5d341f36b19bf6052
Nikita Sirovskiy <nicksirovsky@gmail.com>
2022-01-30 21:06:15 +0200

    Add the chevron icon

--- src/lib/shared_widget/section/src/items/values/leafy_section_chevron_value.dart
+++ src/lib/shared_widget/section/src/items/values/leafy_section_chevron_value.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:leafy_launcher/resources/assets/leafy_icons.dart';
 import 'package:leafy_launcher/resources/theme/leafy_theme.dart';
 import 'package:leafy_launcher/shared_widget/themed_widget.dart';
 
@@ -8,9 +10,10 @@ class LeafySectionChevronValue<TTheme extends LeafyTheme>
 
   @override
   Widget body(BuildContext context, LeafyTheme theme) {
-    return Icon(
-      Icons.chevron_right,
+    return SvgPicture.asset(
+      LeafyIcons.chevronRight,
       color: theme.textInfoColor,
+      height: 12,
     );
   }
 }
