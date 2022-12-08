2e4661bbbe072c6fe953256e9d95d2c503b329c2
Nikita Sirovskiy <nicksirovsky@gmail.com>
2022-12-08 20:31:14 +0300

    Provide docs

--- src/leafy_launcher/lib/presentation/widgets/vibration/device_vibration.dart
+++ src/leafy_launcher/lib/presentation/widgets/vibration/device_vibration.dart
@@ -3,6 +3,7 @@ import 'package:leafy_domain/leafy_domain.dart';
 import 'package:leafy_launcher/injection/injector.dart';
 import 'package:leafy_launcher/presentation/services/ui/device_vibration_service_ui.dart';
 
+/// Is responsible for requesting device vibration.
 class DeviceVibration extends InheritedWidget {
   const DeviceVibration({required super.child});
 
@@ -11,6 +12,7 @@ class DeviceVibration extends InheritedWidget {
     return false;
   }
 
+  /// Will vibrate only if the vibration preference is set to `always`.
   Future<void> vibrateIfIsAlways() {
     return injector<LeafyPreferencesService>().get().then((value) {
       if (value.isVibrateAlways) {
@@ -19,6 +21,10 @@ class DeviceVibration extends InheritedWidget {
     });
   }
 
+  /// Will vibrate only if the vibration preference is either set
+  /// to `always` or `onLaunch`.
+  ///
+  /// Most often used for app launch actions.
   Future<void> vibrateIfEnabled() {
     return injector<LeafyPreferencesService>().get().then((value) {
       if (!value.isVibrateNever) {
